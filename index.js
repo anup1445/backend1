8149e5615eff987bc51f1ad174ba1e54f226e678
anup ghunawat <anupghunawat10@gmail.com>
2025-05-24 13:44:03 +0530

    new route added

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/new', (req, res) => {
+    res.send('new page route');
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
